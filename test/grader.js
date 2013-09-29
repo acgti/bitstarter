3c2d0d28547f401607825f88c825a929ae7a6083
acgti <andy@hatchstud.io>
2013-09-30 00:02:26 +0100

    added HTML parsing variable and command line to add URL

--- test/grader.js
+++ test/grader.js
@@ -27,6 +27,13 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+var HtmlFromUrl = function(URL){
+ //get the data from URL and on completing pass this t
+ rest.get(URL).on('complete',function(result){
+  sys.puts(result);
+ })
+};
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,6 +72,7 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url_to_check','link', HtmlFromUrl)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
