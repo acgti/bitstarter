bc909f5d22c06f96ca05edb2ea9214f63880f122
acgti <andy@hatchstud.io>
2013-09-30 00:24:27 +0100

    Update grader.js

--- grader.js
+++ grader.js
@@ -23,12 +23,23 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
-var rest = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var rest = require('restler')
 var CHECKSFILE_DEFAULT = "checks.json";
 
-//download the html from what's entered in command line//
+
+// Checks to see if the given URL exists. If so, the returns URL in string. If not, then error.
+var assertURLExists = function(url) {
+  var urlstr = url.toString();
+  rest.get(urlstr).on('complete', function(result) {
+    if (result instanceof Error) {
+      console.log('Error: ' + result.message);
+      this.retry(5000); // try again after 5 sec
+    }
+  });
+  return urlstr;
+}
 
 
 
@@ -60,6 +71,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(urlContents, checksfile) {
+    $ = cheerio.load(urlContents);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -68,12 +90,29 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url_to_check','link', clone(assertURLExists))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+        // If file name was given, compare checks to file
+    if(!program.url) {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+ 
+    // If URL was given, compare checks to URL
+    else {
+        // Compare checks to URL
+        rest.get(program.url).on('complete', function(result) {
+            // result is the info in URL
+            var checkJson = checkURL(result, program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+ 
+            console.log(outJson);
+        });
+    }
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
